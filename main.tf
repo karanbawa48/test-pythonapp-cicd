a729224d7d69c109259976b3bbbe7eb05b1653d8
Karan Gir <66402714+karanbawa48@users.noreply.github.com>
2024-03-27 09:49:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Provider configuration (assuming GCP)
 provider "google" {
   # credentials = file("<path-to-your-service-account-key>")
-  project     = "probable-bebop-418220"
-
+  project = "probable-bebop-418220"
 }
+
 # Resource for Google Cloud Run
 resource "google_cloud_run_service" "hello_world" {
   name     = "hello-world"
@@ -22,4 +22,21 @@ resource "google_cloud_run_service" "hello_world" {
     percent         = 100
     latest_revision = true
   }
+}
+  # Configure IAM policy to allow unauthenticated invocations
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.hello_world.location
+  project     = google_cloud_run_service.hello_world.project
+  service     = google_cloud_run_service.hello_world.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
