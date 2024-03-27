d8381651bd31c37b20e9814928e814b26946f29d
Karan Gir <66402714+karanbawa48@users.noreply.github.com>
2024-03-28 02:00:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
     bucket  = backend-tf-state-gcp-1
-    prefix  = "terraform/state"
-    key     = "aws/${var.appname}/${var.env}"
+    prefix  = "terraform/${var.appname}/${var.env}"
   }
 }
