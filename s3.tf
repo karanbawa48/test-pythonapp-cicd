3dc47265396618e2e6ebdccf0f60336578617aaf
Karan Gir <66402714+karanbawa48@users.noreply.github.com>
2024-03-28 00:57:45 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "default" {
-  name          = "backend-tf-state-gcp-prod"
+  name          = "backend-tf-state-gcp-stage"
   force_destroy = true
   location      = "US"
   storage_class = "STANDARD"
