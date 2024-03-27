c071ae3cffd47b09e81ba3ef758d318e4bec29f2
karanzest <karan.an@zest.ai>
2024-03-27 09:25:17 +0530

    updated the unauthenticated invocations

--- main.tf
+++ main.tf
@@ -4,17 +4,22 @@ provider "google" {
   project     = "probable-bebop-418220"
 
 }
-
 # Resource for Google Cloud Run
 resource "google_cloud_run_service" "hello_world" {
   name     = "hello-world"
-   location = "us-east1"
+  location = "us-east1 "
+
   template {
     spec {
       containers {
-        image = "gcr.io/<your-project-id>/hello-world-app:latest"
+        image = "us-east1-docker.pkg.dev/probable-bebop-418220/test-app/web-app"
       }
     }
   }
-}
 
+  # Allow unauthenticated invocations
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
